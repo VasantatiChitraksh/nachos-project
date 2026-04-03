a88a4ddc4d657538b1cd0d347547726614126b4a
Chitraksh <chitravasantati@gmail.com>
2026-04-04 00:01:12 +0530

    Demand Paging completed

--- code/userprog/exception.cc
+++ code/userprog/exception.cc
@@ -499,6 +499,13 @@ void ExceptionHandler(ExceptionType which) {
             DEBUG(dbgSys, "Switch to system mode\n");
             break;
         case PageFaultException:
+        {
+            int faultingAddr = kernel->machine->ReadRegister(BadVAddrReg);
+            cout << "=== Got a Page fault at the address: " << faultingAddr << " ===" << endl;
+            kernel->currentThread->space->LoadPage(faultingAddr);
+            kernel->currentThread->space->RestoreState();
+            return;
+        }
         case ReadOnlyException:
         case BusErrorException:
         case AddressErrorException:
@@ -592,6 +599,9 @@ void ExceptionHandler(ExceptionType which) {
                     break;
             }
             break;
+
+
+
         default:
             cerr << "Unexpected user mode exception" << (int)which << "\n";
             break;
